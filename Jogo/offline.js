0241889ae76af90315e1899de4b4a84247e0d07a
yasmingabrielle <50117540+yasmingabrielle@users.noreply.github.com>
2019-04-29 15:34:16 -0300

    Add files via upload

---
+++ Jogo/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1556561093,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/buttonup-sheet0.png",
+		"images/buttonup-sheet1.png",
+		"images/buttondown-sheet0.png",
+		"images/buttonrigth-sheet0.png",
+		"images/buttonleft-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
